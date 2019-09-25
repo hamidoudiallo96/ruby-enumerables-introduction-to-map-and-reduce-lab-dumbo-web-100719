e6b570bf7d1d51268ebea79a2b5c8735a0a2289a
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-25 04:45:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,12 +62,8 @@ end
 def reduce_to_all_false(source_array)
   total = 0
   i=0
-  while i<source_array.length
   return false if source_array[i] == false
   total+= source_array[i]
-  
-  end
- 
 end
 
 
