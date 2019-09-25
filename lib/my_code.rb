cc02e0b17110335dac71fb6f075f482b17182121
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-25 05:34:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,9 @@ def reduce_to_total(source_array,starting_point=0)
 end
 
 
-def reduce_to_all_true(source_array, starting_point=nil)
+def reduce_to_all_true(source_array)
+  value = true
+  if value = 
   
 end
 
